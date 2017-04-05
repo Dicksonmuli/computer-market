69e2211353754b02d4b6c2c760084c3db3485491
dicksonmuli <dicksonmully6@gmail.com>
2017-04-05 18:52:18 +0300

    list of computer to display- computer tile which displays list of computers

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+		return this.store.findAll('computer');
+  //   return Ember.RSVP.hash({
+  //   computers: this.store.findAll('computer'),
+  //   reviews: this.store.findAll('review')
+  // });
+  },
 });
---
+++ app/models/computer.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	title: DS.attr(),
+  brand: DS.attr(),
+  description: DS.attr(),
+	cost: DS.attr(),
+  image: DS.attr(),
+  processor: DS.attr(),
+  // reviews: DS.hasMany('review', {async: true})
+});
